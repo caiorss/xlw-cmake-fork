7b7341558caaeb50dd3dfac7f54d16eacb890222
CaioRS <caiorss.rodrigues@gmail.com>
2019-01-05 12:04:04 -0800

    Convert example project LoggerDemo to CMake.

---
+++ examples/LoggerDemo/build-msvc.bat
@@ -0,0 +1,24 @@
+:: Build project using Visual Studio tools
+:: (MSBuild and Visual Studio Solutions)
+:: Note: Assumes that CMake is in $PATH variable.
+::====================================================================    
+@echo off
+@echo Building project 
+
+:: Alias to executable
+set CMAKEBIN="C:\Program Files\CMake\bin\cmake"
+
+@echo Configuring project
+@echo --------------------------------------------------
+::  Generate specific preferred project for current platform  
+cmake -H. -Bcache -G "Visual Studio 15 2017" 
+
+@echo Build all targets
+@echo --------------------------------------------------
+:: Build project and copy executable and dll to this directory
+cmake --build cache --target 
+    
+@echo Compilation finished
+::-----------------------------------------------::
+:: Wait user enter any key to exit
+pause 
